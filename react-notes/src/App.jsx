a880f66fadf9aa4d36e5201096ddb64dcaf27194
ItsKiyoto <71987440+ItsKiyoto@users.noreply.github.com>
2026-02-01 19:21:27 +0000

    Asset cleanup and basic note taking developed

--- react-notes/src/App.jsx
+++ react-notes/src/App.jsx
@@ -1,25 +1,88 @@
-import { useState } from 'react'
+import { createElement, useState } from 'react'
 import './App.css'
 
+let noteId = 0;
+let currentId = 0;
+
 function App() {
+  const [writeState, setWriteState] = useState(0);
+  const [notes, setNotes] = useState([]);
+  const [currentText, setText] = useState('');
+
+  function setEditing(){
+    setWriteState(writeState + 1);
+  };
+
+  function createNote(){
+    setNotes([...notes, {id: ++noteId, text : ''}]);
+    currentId = noteId;
+  };
+
+  function saveNote(){
+    const updatedNotes = notes.map(note => {
+      if (note.id === currentId){
+        return ({id: note.id, text: currentText});
+      } else {
+        return note;
+      }
+    })
+    setNotes(updatedNotes)
+  };
+
+  function test(){
+    alert(notes.map(note => note.id === currentId ? note.text : undefined))
+  };
 
   return (
     <>
-      <div>
+      <div className='topbar'>
         <h2>Note.io</h2>
       </div>
-      <div className="welcome-body">  
-      <p>
-        Welcome to Note.io
-      </p>
-      <p>
-        Press the + to start noting down
-      </p>
-      </div>
-      <div className="create">
-        <button onClick={() => alert("Hey the button is working") }>
-          +
-        </button>
+      <div className='welcome'> 
+        {!writeState ? (
+        <div className="welcomeBody">  
+          <p>
+            Welcome to Note.io
+          </p>
+          <p>
+            Press the + to start noting down
+          </p>
+          <div className="create">
+            <button className='createBut' onClick={() => {setEditing(), createNote()}}>
+              +
+            </button>
+          </div>
+        </div>
+        )
+      :
+      (
+        <>
+          {notes.map(note => (
+            <div key={note.id}>  
+                <textarea 
+                  rows={25} 
+                  cols={40}
+                  defaultValue={note.text}
+                  onChange={txt => setText(txt.target.value)}>
+                </textarea>
+            </div>
+          ))}
+          <div className='Navigation'>
+            <button className='saveNote' onClick={() => saveNote()}>
+              Save
+            </button>
+            <button className='prevNote'>
+              Previous
+            </button>
+            <button className='nextNote'onClick={() => test()}>
+              Next
+            </button>
+            <button className='addNote' onClick={() => createNote()}>
+                +
+            </button>
+          </div>
+        </>
+      )}  
       </div>
     </>
   )
